f662e68f198ffd1ab6e131c3a63ef38ba22db7d9
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-26 15:13:04 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/resources/UserResource.java
+++ src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/resources/UserResource.java
@@ -1,21 +1,31 @@
 package com.educandoweb.ProjetoWebServicesJpaHibernate.resources;
 
 import com.educandoweb.ProjetoWebServicesJpaHibernate.entities.User;
+import com.educandoweb.ProjetoWebServicesJpaHibernate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    /*@Autowired
-    private UserService service;*/
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/service/UserService.java
+++ src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/service/UserService.java
@@ -2,16 +2,24 @@ package com.educandoweb.ProjetoWebServicesJpaHibernate.service;
 
 import com.educandoweb.ProjetoWebServicesJpaHibernate.entities.User;
 import com.educandoweb.ProjetoWebServicesJpaHibernate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    UserRepository repo;
+    @Autowired
+    private UserRepository repo;
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repo.findById(id);
+        return obj.get();
     }
 }
