88c670d203b7bcc64b95b4ae7b94845407e87f7d
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-26 09:07:02 -0300

    Project created

---
+++ src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/ProjectWebServicesJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.ProjetoWebServicesJpaHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebServicesJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServicesJpaHibernateApplication.class, args);
+	}
+
+}
