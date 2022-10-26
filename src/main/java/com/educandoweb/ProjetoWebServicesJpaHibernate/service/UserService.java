4cf00eebd2b468d9638e37e10609bd511fca332a
Vinicius Atilio <vinicius-atilio@outlook.com>
2022-10-26 09:49:11 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/ProjetoWebServicesJpaHibernate/service/UserService.java
@@ -0,0 +1,14 @@
+/*package com.educandoweb.ProjetoWebServicesJpaHibernate.service;
+
+import com.educandoweb.ProjetoWebServicesJpaHibernate.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    public List<User> findAll() {
+        return repo.findAll()
+    }
+}
+*/
